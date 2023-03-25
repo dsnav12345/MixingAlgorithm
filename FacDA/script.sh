d35cc94c12f5801a6d3c196b31573691e9fd7325
dsnav12345 <54897311+dsnav12345@users.noreply.github.com>
2023-03-25 16:11:27 +0530

    FacDA version

---
+++ FacDA/script.sh
@@ -0,0 +1,22 @@
+for m in {2..6}
+do
+    for d in {3..5}
+    do
+        for n in {2..5}
+        do
+            inp='./inputs/'${m}'_'${d}'_'${n}'.txt'
+#            oup='./outputs/'${m}'_'${d}'_'${n}'.txt'
+#
+##            id=$((0))
+#            echo -e '' > $oup
+
+            while IFS= read -r line; do
+                python3 FacDA.py $line
+#                id=$(( id + 1 ))
+#                echo $id >> $oup
+            done < "$inp"
+
+            echo ${m} ${d} ${n}
+        done
+    done
+done
